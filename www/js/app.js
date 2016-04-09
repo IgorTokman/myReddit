d5c0463593db5e69fe3268c37e3ad809460c0d2f
IgorTokman <igortokman1994@ukr.net>
2016-04-09 19:14:56 +0300

    added the infinite scroll and pull-to-refresh.

--- www/js/app.js
+++ www/js/app.js
@@ -6,13 +6,37 @@
 
     $scope.stories = [];
 
-    $http.get("https://www.reddit.com/r/Android/new/.json")
-        .success(function (response) {
-          console.log(response.data.children);
-          angular.forEach(response.data.children, function (child) {
-            $scope.stories.push(child.data);
+    function loadStories(params, callback) {
+      $http.get("https://www.reddit.com/r/Android/new/.json", {params: params})
+          .success(function (response) {
+            var stories = [];
+            angular.forEach(response.data.children, function (child) {
+              $scope.stories.push(child.data);
+            });
+        callback(stories);
           });
-        })
+    }
+
+    $scope.loadOlderStories = function () {
+      var params = {};
+      if($scope.stories.length > 0)
+          params['after'] = $scope.stories[$scope.stories.length-1].name;
+
+      loadStories(params, function (olderStories) {
+        $scope.stories = $scope.stories.concat(olderStories);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      })
+    }
+
+    $scope.loadNewerStories = function () {
+      var params = {'before': $scope.stories[0].name};
+
+      loadStories(params, function (newerStories) {
+        $scope.stories = $scope.stories.concat(newerStories);
+        $scope.$broadcast('scroll.refreshComplete');
+      })
+    }
+
   });
 
   app.run(function($ionicPlatform) {
